a4b6caf4a0d5189ad73ba02908115a07a794ca1d
c-note <dongustavo20@gmail.com>
2023-11-09 12:09:19 +0000

    Add sixth task and solution.

---
+++ src/six_task.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+fn main() {
+    // Write a function that implements the logic, 'You can see the movie if you are 17 or older, or if you are 13 or older and have a parent's permission.'
+
+    let args: Vec<String> = env::args().collect();
+
+    let num: i32 = args[1].parse().unwrap();
+    let flag: bool = args[2].parse().unwrap();
+
+    println!("Can see a movie {}", can_see_movie(num, flag));
+}
+
+fn can_see_movie(age: i32, permission: bool) -> bool {
+
+    if age >= 17 {
+        return true;
+    } else if age < 17 && permission {
+        return true;
+    } else {
+        return false;
+    }
+}
