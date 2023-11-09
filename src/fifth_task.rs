37538675b409b0f1be76798a5d764671cd52478b
c-note <dongustavo20@gmail.com>
2023-11-09 11:46:11 +0000

    Update comment.

--- src/fifth_task.rs
+++ src/fifth_task.rs
@@ -3,7 +3,7 @@ fn main() {
     // These triples are commonly written as (a, b, c), and a well-known example is (3, 4, 5).
     // Write a program that computes the Pythagorean triplet such that a < b < c and a + b + c = 1000
 
-    println!("The Pythagorean triplet such that a < b < c and a + b + c = 1000 is{:?}", find_pythagorean_triplet());
+    println!("The Pythagorean triplet such that a < b < c and a + b + c = 1000 is {:?}", find_pythagorean_triplet());
 }
 
 fn find_pythagorean_triplet() -> Vec<i32> {
