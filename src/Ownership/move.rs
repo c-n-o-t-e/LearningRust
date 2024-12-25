c07e5299fe9d0f1b57bf2a7ad347a9b3a5933cd1
c-note <dongustavo20@gmail.com>
2024-12-25 19:15:02 +0000

    Models

--- src/Ownership/move.rs
+++ src/Ownership/move.rs
@@ -3,6 +3,8 @@ fn main() {
     let s1 = String::from("world");
     let s2 = s1.clone();
 
+    let bg: str = "uuuu";
+
     // values stored in stack
     let s3 = 3;
     let s4 = s1;
@@ -12,11 +14,11 @@ fn main() {
 
 fn main() {
     let vec_1 = vec![1, 2, 3];
-    take_ownership(vec_1); // vec_1.clone() to not move 
+    take_ownership(vec_1); // vec_1.clone() to not move
 }
 
 fn take_ownership(vec: Vec<i32>) {
     println("{}", vec);
 }
 
-// give ownership 
+// give ownership
