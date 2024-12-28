88383dd3a34c15bfd4a4fc42f3537810a706515e
c-note <dongustavo20@gmail.com>
2024-12-28 23:03:28 +0000

    closures,iterators,etc.

---
+++ src/bin/function_pointers_task.rs
@@ -0,0 +1,33 @@
+// Problem 1: Complete the function signature for `sum_of_squares`.
+//It must not contain any generics.
+
+fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+fn square(x: u32) -> u32 {
+    x * x
+}
+
+// fn sum_of_squares(num: u32, sq: ?, add: ?) -> u32 {
+//     let mut result = 0;
+//     for i in 1..=num {
+//         result = add(result, sq(i));
+//     }
+//     result
+// }
+
+// Solution
+fn sum_of_squares(num: u32, sq: fn(u32) -> u32, add: fn(u32, u32) -> u32) -> u32 {
+    let mut result = 0;
+    for i in 1..=num {
+        result = add(result, sq(i));
+    }
+    result
+}
+
+fn main() {
+    let num = 4;
+    let sum = sum_of_squares(num, square, add);
+    println!("Sum of squares from 1 to {} = {}", num, sum);
+}
